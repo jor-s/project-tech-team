deb267e32bd63890417bd453f84d19fd417f600a
Jordy S <jordy.sahit@live.nl>
2020-03-23 15:36:58 +0100

    corrected express static

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const port = 3000;
+const express = require('express')
+const bodyParser = require('body-parser')
+const port = 3000
 
 express()
-  .use(express.static('static'));
-  .use(bodyParser.urlencoded({extended: true}));
-  .set('view engine', 'ejs');
-  .set('views', 'views');
-  .get('/', index);
+  .use('/static', express.static('static'))
+  .use(bodyParser.urlencoded({extended: true}))
+  .set('view engine', 'ejs')
+  .set('views', 'views')
+  .get('/', index)
   .listen(port, () => console.log('Listening on port ' + port))
 
 
 function index (req, res) {
-  res.send('Hello world');
+  res.send('Hello world')
 }
