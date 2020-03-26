5138367c2435824e9d1d26a9b3662651f0e4b10e
Azam Awan <admin@Azams-MacBook-Pro.local>
2020-03-26 13:06:56 +0100

    Added .ejs files

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const port = 3000
 
+const app = express()
+
 express()
   .use('/public', express.static('static'))
   .use(bodyParser.urlencoded({extended: true}))
@@ -11,6 +13,23 @@ express()
   .listen(port, () => console.log('Listening on port ' + port))
 
 
+
+
+
 function index (req, res) {
   res.send('Hello world')
 }
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+})
+
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.ejs');
+})
+
+
