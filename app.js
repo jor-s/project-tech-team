6afa1accad13f14b5c7f4b53aaef7bdb0e8a6883
Chun <45426792+ChunningTatum@users.noreply.github.com>
2020-03-26 14:02:43 +0100

    problem with loading css partial routes fixed

--- app.js
+++ app.js
@@ -2,34 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const port = 3000
 
+const router = require('./routes/index.js')
+
 const app = express()
 
 express()
   .use('/public', express.static('static'))
   .use(bodyParser.urlencoded({extended: true}))
+  .use('/', router)
   .set('view engine', 'ejs')
   .set('views', 'views')
-  .get('/', index)
   .listen(port, () => console.log('Listening on port ' + port))
-
-
-
-
-
-function index (req, res) {
-  res.send('Hello world')
-}
-
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-})
-
-app.get('/index', (req, res) => {
-  res.render('index.ejs');
-})
-
-app.get('/about', (req, res) => {
-  res.render('about.ejs');
-})
-
-
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res) => {
+  res.render('index.ejs');
+})
+
+router.get('/login', (req, res) => {
+  res.render('login.ejs');
+})
+
+router.get('/register', (req, res) => {
+  res.render('register.ejs');
+})
 
 module.exports = router;
