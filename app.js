2bbd86904cf8ddf379fcfc709adedb6e86227432
Jordy S <jordy.sahit@live.nl>
2020-03-26 14:51:51 +0100

    set up database part 2

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const port = 3000
-
 const router = require('./routes/index.js')
-
 const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
 
-express()
+app
   .use('/public', express.static('public'))
   .use(bodyParser.urlencoded({extended: true}))
   .use('/', router)
   .set('view engine', 'ejs')
   .set('views', 'views')
   .listen(port, () => console.log('Listening on port ' + port))
+
+//connect with database
+mongoose.connect(process.env.MONGODB_URI, {
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS,
+  useNewUrlParser: 'true',
+  useUnifiedTopology: 'true',
+  useCreateIndex: 'true'
+})
+
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  console.log("DATABASE CONNECTED FOR SURE")
+})
