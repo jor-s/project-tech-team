832e397356eda63414e7f95ee642a854d2c82ae8
Jordy S <jordy.sahit@live.nl>
2020-03-27 12:13:26 +0100

    added edit-profile, made html and css

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,15 @@ var router = express.Router();
 const profileController = require('../helpers/profile')
 
 router.get('/', profileController.home)
+router.get('/login', profileController.logIn)
 router.post('/login', profileController.doLogin)
 
-router.get('/register', profileController.doRegister)
+router.get('/register', profileController.goToRegister)
 router.post('/register', profileController.doRegister)
 
 router.get('/profile', profileController.profile)
+
+router.get('/edit-profile', profileController.goToEdit)
+router.post('/edit-profile', profileController.doEdit)
+
 module.exports = router;
