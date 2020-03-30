b4adf3ebb1d2ea4c1b616c7009340e070f05ba88
Jordy S <jordy.sahit@live.nl>
2020-03-30 16:58:41 +0200

    Revert "ratelimiter not working anymore, checking if i can get it working"
    This reverts commit 0f94fb019999e24121cf9429cdab630da2e951df.

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,19 @@ var router = express.Router()
 const profileController = require('../controllers/profile')
 const passport = require('passport')
 const imageController = require('../config/multer')
-const bruteforceCheck = require('../config/ratelimiter')
-const editController = require('../controllers/edit-profile')
+const bruteforceCheck = require("../config/ratelimiter")
 
 router.get('/', profileController.home)
 
 router.get('/login', profileController.logIn)
-router.post('/login', bruteforceCheck.loginLimiter, profileController.doLogin, passport.authenticate('local', {failureRedirect: '/login'} ))
+router.post('/login', bruteforceCheck.loginLimiter, profileController.doLogin, passport.authenticate('local', {failureRedirect: '/login'}))
 
 router.get('/register', profileController.goToRegister)
 router.post('/register', profileController.doRegister)
 
 router.get('/profile', profileController.profile)
 
-router.get('/edit-profile', editController.goToEdit)
-router.post('/edit-profile', imageController.upload.single('picture'), editController.doEdit)
+router.get('/edit-profile', profileController.goToEdit)
+router.post('/edit-profile', imageController.upload.single('picture'), profileController.doEdit)
 
 module.exports = router;
