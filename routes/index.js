852f42ab560d4e5440acced51f2f685e0a06f48b
Chun <45426792+ChunningTatum@users.noreply.github.com>
2020-04-03 11:29:21 +0200

    push routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,29 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const profileController = require('../controllers/profile')
+const passport = require('passport')
+const imageController = require('../config/multer')
+const bruteforceCheck = require("../config/ratelimiter")
 
-router.get('/', (req, res) => {
-  res.render('index.ejs');
-})
+router.get('/', profileController.home)
 
-router.get('/login', (req, res) => {
-  res.render('login.ejs');
-})
+router.get('/login', profileController.logIn)
+router.post('/login', bruteforceCheck.loginLimiter, passport.authenticate('local', {  failureRedirect: '/login'}), profileController.doLogin)
 
-router.get('/register', (req, res) => {
-  res.render('register.ejs');
-})
+
+// Logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  //req.flash('success_msg', 'You are logged out');
+  res.redirect('/login');
+});
+
+router.get('/register', profileController.goToRegister)
+router.post('/register', profileController.doRegister)
+
+router.get('/profile', profileController.profile)
+
+router.get('/edit-profile', profileController.goToEdit)
+router.post('/edit-profile', imageController.upload.single('picture'), profileController.doEdit)
 
 module.exports = router;
