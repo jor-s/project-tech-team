97c121ac2f20a6fb49f2bb3fcdea35568dd915b7
Chun <45426792+ChunningTatum@users.noreply.github.com>
2020-04-03 11:26:36 +0200

    Azam changes

---
+++ config/passport.js
@@ -0,0 +1,54 @@
+const LocalStrategy = require('passport-local').Strategy
+const passport = require('passport')
+const mongoosedata = require('../models/user')
+
+passport.use(new LocalStrategy( {usernameField: 'email'},
+  function (username, password, done) {
+    mongoosedata.findOne({
+      email: username
+    }, function (err, user) {
+      console.log('logging userdata in local strategy: ', user)
+      console.log('client entered the following password: ', password)
+      if (err) {
+        return done(err)
+      }
+      if (!user) {
+        return done(null, false, console.log('credentials incorrect'))
+      }
+
+      return done(null, user)
+    })
+  }
+))
+
+module.exports = {
+  ensureAuthenticated: function(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    req.flash('error_msg', 'Please log in to view that resource');
+    res.redirect('/login');
+  },
+  forwardAuthenticated: function(req, res, next) {
+    if (!req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/login');
+  }
+};
+
+
+passport.serializeUser(function (user, done) {
+  console.log('serializing: ' + user.id)
+  done(null, user.id)
+})
+
+
+passport.deserializeUser(function (id, done) {
+  console.log('deserializing: ' + id)
+  mongoosedata.findById(id, function (err, user) {
+    done(err, user)
+  })
+})
+
+module.exports = passport
---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+let schema = mongoose.Schema
+
+let userSchema = new schema({
+  name: String,
+  email: String,
+  age: Number,
+  password: String,
+  date: String,
+  preferences: String,
+  hobby: Array,
+  picture: String,
+  pickupline: String
+})
+
+let user = mongoose.model('User', userSchema)
+
+module.exports = user
