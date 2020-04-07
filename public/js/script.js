60209479bb84e13a4cd6e77d8da5a2c10f952dc0
Chun <45426792+ChunningTatum@users.noreply.github.com>
2020-04-07 17:02:03 +0200

    Implemented bcrypt compare password

--- public/js/script.js
+++ public/js/script.js
@@ -8,43 +8,43 @@ cases();
 
 let nextButton = document.querySelector('.next');
 nextButton.addEventListener('click', function (){
-    counter++;
-    console.log(counter);
-    cases();
-    if(counter > 2){
-        counter--;
-    }
+	counter++;
+	console.log(counter);
+	cases();
+	if(counter > 2){
+		counter--;
+	}
 });
 
 let backButton = document.querySelector('.back');
 backButton.addEventListener('click', function () {
-    counter--;
-    console.log(counter);
-    cases();
-    if(counter == -1){
-        counter++;
-    }
+	counter--;
+	console.log(counter);
+	cases();
+	if(counter == -1){
+		counter++;
+	}
 });
 
 console.log(counter);
 
 function cases(){
 
-switch (counter) {
-    case 0:
-        personalia.classList.replace('hidden', 'personalia');
-        pickupline.classList.replace('pickupline', 'hidden');
-        uploadPicture.classList.add('hidden');
-        break;
-    case 1:
-        personalia.classList.replace('personalia', 'hidden');
-        pickupline.classList.replace('hidden', 'pickupline');
-        uploadPicture.classList.add('hidden');
-        break;
-    case 2:
-        personalia.classList.replace('personalia', 'hidden');
-        pickupline.classList.replace('pickupline', 'hidden');
-        uploadPicture.classList.toggle('hidden');
-        break;
+	switch (counter) {
+	case 0:
+		personalia.classList.replace('hidden', 'personalia');
+		pickupline.classList.replace('pickupline', 'hidden');
+		uploadPicture.classList.add('hidden');
+		break;
+	case 1:
+		personalia.classList.replace('personalia', 'hidden');
+		pickupline.classList.replace('hidden', 'pickupline');
+		uploadPicture.classList.add('hidden');
+		break;
+	case 2:
+		personalia.classList.replace('personalia', 'hidden');
+		pickupline.classList.replace('pickupline', 'hidden');
+		uploadPicture.classList.toggle('hidden');
+		break;
+	}
 }
-    }
